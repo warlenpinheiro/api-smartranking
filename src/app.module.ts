bf9a157d90886d9535db147a8623b50a122f5330
Warlen Pinheiro <warlen@live.com>
2020-10-25 16:32:56 -0300

    update nome

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:eH9zl7ArLT2q58CZ@cluster0.qvvps.mongodb.net/smartranking?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }
+    ),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+
+export const JogadorSchema = new mongoose.Schema({
+    telefoneCelular: { type: String, unique: true },
+    email: { type: String, unique: true },
+    nome: String,
+    ranking: String
+});
