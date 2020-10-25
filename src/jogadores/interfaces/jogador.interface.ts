df123b2ff9b3bf477eb3e75af49f60e178b3502b
Warlen Pinheiro <warlen@live.com>
2020-10-25 16:36:36 -0300

    Add Mongoose

--- src/jogadores/interfaces/jogador.interface.ts
+++ src/jogadores/interfaces/jogador.interface.ts
@@ -1,4 +1,5 @@
-export interface Jogador {
+import { Document } from "mongoose";
+export interface Jogador extends Document {
     readonly _id: string;
     readonly telefoneCelular: string;
     readonly email: string;
--- src/jogadores/interfaces/jogador.schema.ts
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -4,5 +4,7 @@ export const JogadorSchema = new mongoose.Schema({
     telefoneCelular: { type: String, unique: true },
     email: { type: String, unique: true },
     nome: String,
-    ranking: String
-});
+    ranking: String,
+    posicaoRanking: Number,
+    urlFotoJogador: String,
+}, {timestamps: true, collection: 'jogadores'});
